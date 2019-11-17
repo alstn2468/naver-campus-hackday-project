86cf24907ce9a710b9a93934b6d76f68b3a25d8b
김민수 <alstn2468_@naver.com>
2019-11-17 21:25:20 +0900

    Increasing the unauthenticated rate limit for OAuth applications using client_id & client_secret

--- hackdayproject/main/views.py
+++ hackdayproject/main/views.py
@@ -3,35 +3,46 @@ from django.contrib.auth.forms import AdminPasswordChangeForm, PasswordChangeFor
 from django.contrib.auth import update_session_auth_hash
 from django.contrib import messages
 from django.shortcuts import render, redirect
-import requests
 from social_django.models import UserSocialAuth
+from django.conf import settings as conf_settings
+import requests
 
 
 def home(request):
     user = request.user
 
     if user:
-        user_repos = requests.get(
-            'https://api.github.com/users/' + user.username + '/repos', {
-                "type": "all",
-                "sort": "updated"
-            }).json()
+        try:
+            suffix = "?client_id=" + conf_settings.SOCIAL_AUTH_GITHUB_KEY + \
+                "&client_secret=" + conf_settings.SOCIAL_AUTH_GITHUB_SECRET
 
-        user_data = requests.get(
-            "https://api.github.com/users/" + user.username).json()
+            user_repos = requests.get(
+                'https://api.github.com/users/' + user.username + '/repos' + suffix, {
+                    "type": "all",
+                    "sort": "updated"
+                }).json()
 
-        user_data = {
-            "avatar_url": user_data["avatar_url"],
-            "company": user_data["company"],
-            "blog_url": user_data["blog"],
-            "location": user_data["location"],
-            "bio": user_data["bio"],
-            "public_repos_count": user_data["public_repos"],
-            "followers": user_data["followers"],
-            "following": user_data["following"]
-        }
+            user_data = requests.get(
+                "https://api.github.com/users/" + user.username + suffix).json()
 
-        print(user_data)
+            print(user_data)
+
+            user_data = {
+                "avatar_url": user_data["avatar_url"],
+                "company": user_data["company"],
+                "blog_url": user_data["blog"],
+                "location": user_data["location"],
+                "bio": user_data["bio"],
+                "public_repos_count": user_data["public_repos"],
+                "followers": user_data["followers"],
+                "following": user_data["following"]
+            }
+
+        except Exception as e:
+            print(e)
+            user_data = {
+                "error": "Can't get user data."
+            }
 
     return render(request, 'main/home.html', {
         "user_data": user_data
